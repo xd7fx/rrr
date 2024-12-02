ce8c074b6754a4052b55a13f355dd0b0abccc6d6
Abdulrahman Alnashri <d7.fx99@gmail.com>
2024-12-03 00:26:13 +0300

    Create download_model.py

---
+++ download_model.py
@@ -0,0 +1,14 @@
+import gdown
+from pathlib import Path
+
+def download_model():
+    url = "https://drive.google.com/file/d/1Ee25CYBjFowp_JBMQhiezYOiY31e5dvc/view?usp=sharing"
+    output = Path("models/model.pt")
+    if not output.exists():
+        print("Downloading the model...")
+        gdown.download(url, str(output), quiet=False)
+    else:
+        print("Model already exists!")
+
+if __name__ == "__main__":
+    download_model()
