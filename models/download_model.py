bc3561c1fc507cb0c1e9d6dd6a52608cdb0f280c
Abdulrahman Alnashri <d7.fx99@gmail.com>
2024-12-03 01:21:55 +0300

    Update download_model.py

--- models/download_model.py
+++ models/download_model.py
@@ -2,7 +2,7 @@ import gdown
 from pathlib import Path
 
 def download_model():
-    url = "https://drive.google.com/file/d/1Ee25CYBjFowp_JBMQhiezYOiY31e5dvc/view?usp=sharing"
+    url = "https://drive.google.com/file/d/1Ee25CYBjFowp_JBMQhiezYOiY31e5dvc/view?usp=sharing" 
     output = Path("models/model.pt")
     if not output.exists():
         print("Downloading the model...")
